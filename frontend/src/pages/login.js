a88849033ac724f7a3043b5c4339a82c5422b22a
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-01-20 15:06:48 +0530

    feat: login page done

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,39 +1,151 @@
 import React from "react";
+import { styled } from "@mui/material/styles";
 import { Theme } from "../assets/theme.js";
-import { Grid, Box, Stack, TextField } from "@mui/material";
+import { Grid, Box, Typography, TextField, Button } from "@mui/material";
+
+const CssTextField = styled(TextField)({
+    label: {
+      color: "#fff",
+    },
+    "&.MuiTextField-root": {
+        backgroundColor: "#165C3F",
+    },
+    "& label.Mui-focused": {
+      color: "#fff",
+    },
+    "& .MuiOutlinedInput-root": {
+      color: "#fff",
+      "& fieldset": {
+        borderColor: "#000",
+      },
+      "&:hover fieldset": {
+        borderColor: "#fff",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#fff",
+      },
+      "& input[type=number]": {
+        "-moz-appearance": "textfield",
+      },
+      "& input[type=number]::-webkit-outer-spin-button": {
+        "-webkit-appearance": "none",
+        margin: 0,
+      },
+      "& input[type=number]::-webkit-inner-spin-button": {
+        "-webkit-appearance": "none",
+        margin: 0,
+      },
+    },
+  });
+
 const Login = () => {
   return (
-    <Theme>
-      <Grid container justifyContent="center">
-        <Grid item mobile={12} tablet={8.5} laptop={5}>
-          <Box
-            component="form"
-            sx={{
-              width: "100%",
-              minHeight: { mobile: "100vh", tablet: "auto", laptop: "auto" },
-              backgroundColor: {
-                mobile: "primary.main",
-                tablet: "secondary.main",
-                laptop: "secondary.main",
-              },
-              my: "50vh",
-              p: 5,
-              py: { mobile: 14, tablet: 5, laptop: 5 },
-              transform: "translateY(-50%)",
-              overflow: "hidden",
-            }}
-          >
-            <Stack spacing={3}>
-              <TextField
-                id="outlined-basic"
-                label="username"
-                variant="outlined"
-              />
-            </Stack>
-          </Box>
+    <>
+      <Theme>
+        <Grid container justifyContent="center">
+          <Grid item mobile={12} tablet={8.5} laptop={5}>
+            <Box
+              component="form"
+              sx={{
+                width: "100%",
+                minHeight: { mobile: "100vh", tablet: "auto", laptop: "auto" },
+                backgroundColor: {
+                  mobile: "primary.main",
+                  tablet: "secondary.main",
+                  laptop: "secondary.main",
+                },
+                my: "50vh",
+                p: 5,
+                py: { mobile: 20, tablet: 5, laptop: 5 },
+                transform: "translateY(-50%)",
+                overflow: "hidden",
+              }}
+            >
+              <Typography
+                variant="h3"
+                component="h2"
+                color="primary.contrastText"
+                sx={{
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  mt: { mobile: 2, tablet: 5, laptop: 5 },
+                  mb: 2,
+                  textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+                  fontSize: "2.5rem",
+                }}
+              >
+                Welcome Back
+              </Typography>
+              <Typography
+                variant="h6"
+                color="primary.contrastText"
+                sx={{
+                  fontStyle: "italic",
+                  letterSpacing: "0.1rem",
+                  textAlign: "center",
+                  textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+                  mb: 8,
+                }}
+              >
+                “Log into your account”
+              </Typography>
+              <Grid container justifyContent="center" spacing={3}>
+                <Grid item mobile={11} tablet={8.5} laptop={4}>
+                  <CssTextField
+                    id="outlined-basic"
+                    label="Username"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item mobile={11} tablet={8.5} laptop={4}>
+                  <CssTextField
+                    id="outlined-basic"
+                    label="Password"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+              </Grid>
+              <Typography
+                variant="h6"
+                color="primary.contrastText"
+                sx={{
+                  fontStyle: "italic",
+                  fontSize: "0.65rem",
+                  textAlign: "center",
+                  textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+                  mt: 5,
+                }}
+              > By logging in you agree to our terms and conditions</Typography>
+              <div className="flex flex-col sm:flex-row justify-center items-center mt-20">
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "custom.main",
+                  color: "custom.contrastText",
+                  width: { mobile: "90%", tablet: "40%", laptop: "40%" },
+                }}
+              >
+                Sign Up
+              </Button>
+              <Typography
+                variant="h6"
+                color="primary.contrastText"
+                sx={{
+                  fontStyle: "italic",
+                  fontSize: "0.65rem",
+                  textAlign: "center",
+                  textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+                  mt: 1,
+                }}
+              > Don't have an account Signup</Typography>
+              </div>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Theme>
+      </Theme>
+    </>
   );
 };
 
--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -134,7 +134,7 @@ const Signup = () => {
                   mt: 5,
                 }}
               > By registering you agree to our terms and conditions</Typography>
-              <div className="flex flex-col sm:flex-row justify-center items-center gap-5 mt-20">
+              <div className="flex flex-col sm:flex-row justify-center items-center mt-20">
               <Button
                 variant="contained"
                 sx={{
@@ -145,6 +145,17 @@ const Signup = () => {
               >
                 Sign Up
               </Button>
+              <Typography
+                variant="h6"
+                color="primary.contrastText"
+                sx={{
+                  fontStyle: "italic",
+                  fontSize: "0.65rem",
+                  textAlign: "center",
+                  textShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+                  mt: 1,
+                }}
+              > Already have an account Login</Typography>
               </div>
             </Box>
           </Grid>
