e88abca36e2ed65d706623655f56360b66d3514a
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-01-20 22:19:47 +0530

    feat: profile setup

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Theme } from "../assets/theme.js";
+import { Grid, Box } from "@mui/material";
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <>
+      <Theme>
+        <Grid container justifyContent="center">
+          <Grid item mobile={12} tablet={8.5} laptop={5}>
+            <Box
+              component="form"
+              sx={{
+                width: "100%",
+                minHeight: { mobile: "100vh", tablet: "auto", laptop: "auto" },
+                backgroundColor: {
+                  mobile: "primary.main",
+                  tablet: "secondary.main",
+                  laptop: "secondary.main",
+                },
+                my: "50vh",
+                p: 5,
+                py: { mobile: 20, tablet: 5, laptop: 5 },
+                transform: "translateY(-50%)",
+                overflow: "hidden",
+              }}
+            >
+              
+            </Box>
+          </Grid>
+        </Grid>
+      </Theme>
+    </>
   )
 }
