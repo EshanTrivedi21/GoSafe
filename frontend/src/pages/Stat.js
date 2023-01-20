33a8acb515705781c6d919904afa7b45e0afd8c2
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-01-21 00:28:11 +0530

    feat: bmc map dashboard

--- frontend/src/pages/Stat.js
+++ frontend/src/pages/Stat.js
@@ -3,7 +3,6 @@ import React from "react";
 const Stat = () => {
   return (
     <div className="flex flex-row justify-center gap-20">
-      {/* {image} */}
       <div className="flex justify-center items-start flex-col">
         <img
           src="https://picsum.photos/200/300"
@@ -12,21 +11,18 @@ const Stat = () => {
             height: "100px",
             width: "100px",
             borderRadius: "10px",
-            // border: "5px solid hwb(141deg 19% 0% / 87%)",
           }}
         />
         <p className="text-white">RQI analysis :- </p>
         <p className="text-white text-sm">Road Conditions = </p><span className="text-red">Severe</span>
       </div>
-      {/* info */}
       <div>
         <div className="flex flex-col items-start text-white">
           <h1 className="text-4xl font-semibold">Kalyan</h1>
           <p>Sector 44 tandel apartment road Kalyan 400768</p>
           <br />
           <p>
-            Reported By user{" "}
-            <span className="text-text font-bold underline">BHAVNA WAGH</span>
+            Reported By user <span className="text-text font-bold underline">BHAVNA WAGH</span>
           </p>
         </div>
       </div>
